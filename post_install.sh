9e880f889b8462e31155668e89c05f48f087edb1
stefanschramek <stefanschramek@users.noreply.github.com>
2019-11-24 20:34:06 +0100

    Updated post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#sysrc -f /etc/rc.conf unifi_enable="YES"
-#service unifi start 2>/dev/null
+sysrc -f /etc/rc.conf unifi_enable="YES"
+service unifi start 2>/dev/null
 
-#echo "UniFi Controller successfully installed" > /root/PLUGIN_INFO
+echo "UniFi Controller successfully installed" > /root/PLUGIN_INFO
